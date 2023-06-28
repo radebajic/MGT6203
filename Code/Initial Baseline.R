aaf687875c7c7e26a73ac738f30cdf7d6b0597ae
Stijn <68353660+Bovenbaas@users.noreply.github.com>
2023-06-28 22:42:01 +0200

    Add files via upload

---
+++ Code/Initial Baseline.R
@@ -0,0 +1,18 @@
+library(stats)
+
+d = read.csv('commodity-prices-2016.csv')   #Create dataframe from data
+head(d)     #Explore data
+a = ts(d$Agricultural.Raw.Materials.Index, frequency = 12) #Give a shorter name to the agricultural raw materials index
+date = as.Date(d$Date)                    #Create date array from the date data
+
+model = HoltWinters(a,seasonal = 'additive')
+
+model$SSE
+model$fitted
+
+plot(date,a, col = 'red') #Plot the original data in red
+
+b = data.frame(model$fitted)  #Convert the results of the smoothing into a data frame
+points(date[13:length(date)],b$xhat, col = 'green') # plot the smoothing data in green
+
+
