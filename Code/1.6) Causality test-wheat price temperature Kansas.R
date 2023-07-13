9e51149743179732f15643b54faf608a22cb1d21
Stijn <68353660+Bovenbaas@users.noreply.github.com>
2023-07-13 23:22:44 +0200

    Add files via upload

---
+++ Code/1.6) Causality test-wheat price temperature Kansas.R
@@ -0,0 +1,28 @@
+library(lmtest)
+library(stats)
+library(dplyr)
+library(lubridate)
+library(tidyverse)
+
+d = read.csv('average_monthly_temperature_by_state_1950-2022.csv') #Load in data
+d2 = read.csv('commodity-prices-2016.csv') #Load in data
+head(d2)
+
+d$Date = paste(as.character(d$year),as.character(d$month),"01",sep="-") #Convert month to first date of the month
+d$Date = as.Date(d$Date) #Change to date type
+
+d2$Date = as.Date(d2$Date)  #Change to date type
+
+d = d[(d$state == 'Kansas'),] #Filter for illinois data
+
+df1 = subset(d, select = c(Date,average_temp)) #Create dataframe with relevant data from weather dataframe
+df2 = subset(d2, select = c(Date,Wheat)) #Create dataframe with relevant data from price dataframe
+
+
+p = merge(df1,df2,by = 'Date')
+for (i in 1:15){
+model = grangertest(p$Wheat~p$average_temp, order = i)
+print(i)
+print(model)}
+
+
